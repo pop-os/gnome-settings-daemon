4d854ac9c7148d82e8ed5924ff2b607033203edc
robert.ancell@gmail.com <>
2010-06-10 13:02:38 +1000

    * Merge with Debian unstable, remaining Ubuntu changes: * debian/control.in:   - Drop libxrandr-dev and libxrender-dev build-deps   - Build-depend on libpulse and libcanberra   - Build-depend on libappindicator-dev   - Add Vcs-Bzr link   - Recommend pulseaudio * debian/rules:   - Run autotools on build   - Don't disable the pulseaudio use   - Build and install gnome-update-wallpaper-cache * debian/gnome-settings-daemon.gconf-defaults:   - Enable tap_on_click and disable_while_typing on by default,     it's the less confusing behaviour for user and was the consensus     during the discussion on the lists   - Ensure that touchpad settings are migrated to the new     config where appropriate   - Use subpixel rendering and slight hinting as the default * debian/gnome-settings-daemon.install:   - Don't move the autostart to a non standard directory as debian   - Install dbus service   debian/gnome-settings-daemon.postinst:   - Don't move the autostart to a non standard directory as debian * debian/gnome-update-cache-wallpaper.c:   - enable creating a cached wallpaper even when g-s-d or nautilus isn't     running. Used in ubiquity in install mode * debian/patches/02_fix_randr.patch:   - Correctly apply the xrandr settings. * debian/patches/05_disable_corner_tapping.patch:   - Disable corner tapping when disabling tap to click * debian/patches/06_use_application_indicator.patch:   - Use the new application indicator system * debian/patches/07_dont_display_autostart.patch:   - Don't show the gnome-settings-daemon autostart in the session capplet,     the description is confusing and it's a required component * debian/patches/10_fn-F7.patch:   - Fix handling of fn-F7 (xrandr) when xev timestamp is later than server     timestamp * debian/patches/16_use_synchronous_notifications.patch:   - Use synchronous notifications when they are supported * debian/patches/20_gstreamer.patch:   - Don't use this debian change but rather the upstream code version * debian/patches/40_xres_lcddefault.patch:   - OpenOffice.org subpixel font rendering broken with new cairo * debian/patches/70_migrate_touchpad_config.patch:   - Ensure that touchpad settings are migrated to the new     config where appropriate * debian/patches/70_relibtoolize.patch:   - Not required, now run autotools on build * debian/patches/71_fix_ldsm_notification_crash.patch:   - Don't access free'd memory if a volume is unmounted whilst the     dialog is displayed * debian/patches/91_bugzilla_update_touchpad_icons.patch:   - Use correct touchpad svg icons * debian/patches/91_update_gvc_source.patch:   - Update gvc copy using the current gnome-media version to fix a leak issue * debian/watch:   - Watch unstable series
    (bzr r100)

--- debian/gnome-update-wallpaper-cache.c
+++ debian/gnome-update-wallpaper-cache.c
@@ -15,6 +15,8 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
  * Authored by Didier Roche <didrocks@ubuntu.com>
+ * 
+ * Bug: https://bugs.launchpad.net/bugs/530024
  */
 
 #include <glib.h>
