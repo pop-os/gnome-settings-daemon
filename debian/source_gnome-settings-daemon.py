2b6c3a3721765da39dcd0d5fe1de30fcfd78d6f6
Marco Trevisan (Treviño) <mail@3v1n0.net>
2021-05-31 20:49:21 +0200

    debian/source_gnome-settings-daemon.py: Compute libdir version number dynamically
    The g-s-d library version will now change at each major version update,
    so compute it from the package version instead of hardcoding it.

--- debian/source_gnome-settings-daemon.py
+++ debian/source_gnome-settings-daemon.py
@@ -1,15 +1,17 @@
-import os, apport.packaging, re
+import apport.packaging, re
 from apport.hookutils import *
 
 def add_info(report):
 	# the issue is not in the gnome-settings-daemon code so reassign
-	if "Stacktrace" in report and "/usr/lib/gnome-settings-daemon-3.0" in report["Stacktrace"]:
+	major_version = apport.packaging.get_version('gnome-settings-daemon').split('.')[0]
+	gsd_libdir = '/usr/lib/gnome-settings-daemon-{}'.format(major_version)
+	if "Stacktrace" in report and gsd_libdir in report["Stacktrace"]:
 		for words in report["Stacktrace"].split():
-			if words.startswith("/usr/lib/gnome-settings-daemon-3.0"):
+			if words.startswith(gsd_libdir):
 			    if apport.packaging.get_file_package(words) != 'gnome-settings-daemon':
     				report.add_package_info(apport.packaging.get_file_package(words))
-    				return    			
-    		    # update the title to reflect the component and tab	
+    				return
+    		    # update the title to reflect the component and tab
 			    component = re.compile("lib(\w*).so").search(words).groups(1)[0]
 			    report['Title'] = '[%s]: %s' % (component, report.get('Title', report.standard_title()))
 			    report['Tags'] = '%s %s' % (report.get('Tags', ""), component)
